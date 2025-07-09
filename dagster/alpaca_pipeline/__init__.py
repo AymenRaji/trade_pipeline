542332cf8bb6e3c713ffe796a90f42ebc7d9183e
rajiaymen <rajiaymen2@gmail.com>
2025-07-08 22:16:50 -0400

    added job and automated it

--- dagster/alpaca_pipeline/__init__.py
+++ dagster/alpaca_pipeline/__init__.py
@@ -4,6 +4,8 @@ from dagster_embedded_elt.dlt import DagsterDltResource
 
 
 from .assets import alpaca_dlt_asset
+from .jobs import Alpaca_ELT_job
+from .schedules import Alpaca_ELT_schedule
 
 alpaca_dlt_asset = load_assets_from_modules([alpaca_dlt_asset])
 
@@ -16,5 +18,7 @@ defs = Definitions(
     assets=all_assets,
     resources={
         "dlt":dlt_resource
-    }
+    },
+    jobs=[Alpaca_ELT_job],
+    schedules=[Alpaca_ELT_schedule]
 )
