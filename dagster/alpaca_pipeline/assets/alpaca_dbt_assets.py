aab9c5c2fcc9e0900903fe8b310da9e2d279f7a4
rajiaymen <rajiaymen2@gmail.com>
2025-07-14 00:14:31 -0400

    added dbt asset

---
+++ dagster/alpaca_pipeline/assets/alpaca_dbt_assets.py
@@ -0,0 +1,19 @@
+from dagster import AssetExecutionContext
+from dagster_dbt import DagsterDbtTranslator, DbtCliResource, DbtProject, dbt_assets
+from typing import Any, Optional, Mapping
+from pathlib import Path
+
+
+class CustomDagsterDbtTranslator(DagsterDbtTranslator):
+        def get_group_name(
+            self, dbt_resource_props: Mapping[str, Any]
+        ) -> Optional[str]:
+            return "dbt_alpaca"
+
+
+@dbt_assets(
+    manifest=Path("dbt/target/", "manifest.json"),
+    dagster_dbt_translator=CustomDagsterDbtTranslator()
+)
+def alpaca_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
+    yield from dbt.cli(["build"], context=context).stream()
