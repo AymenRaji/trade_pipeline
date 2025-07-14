7b5de537ad0daaaf3099c546342429ced9072177
rajiaymen <rajiaymen2@gmail.com>
2025-07-14 00:16:33 -0400

    add dbt_asset to ELT job

--- dagster/alpaca_pipeline/jobs.py
+++ dagster/alpaca_pipeline/jobs.py
@@ -1,7 +1,7 @@
 from dagster import AssetSelection, define_asset_job
 
 
-Alpaca_ELT_assets = AssetSelection.groups("alpaca_trade")
+Alpaca_ELT_assets = AssetSelection.groups("alpaca_trade") | AssetSelection.groups("dbt_alpaca")
 Alpaca_ELT_job = define_asset_job(
     "Alpaca_ELT_jobs",
     selection=Alpaca_ELT_assets.downstream()
