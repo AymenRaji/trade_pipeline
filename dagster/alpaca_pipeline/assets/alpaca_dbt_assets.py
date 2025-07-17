2f076e76e895f2165d183775e76fd4cbf1ee382f
rajiaymen <rajiaymen2@gmail.com>
2025-07-17 00:48:11 -0400

    added dagsterDltTranslater return correct dlt asset

--- dagster/alpaca_pipeline/assets/alpaca_dbt_assets.py
+++ dagster/alpaca_pipeline/assets/alpaca_dbt_assets.py
@@ -5,16 +5,14 @@ from pathlib import Path
 
 
 class CustomDagsterDbtTranslator(DagsterDbtTranslator):
-    def __init__(self, group_name: str):
-        self.group_name = group_name
-
-    def get_group_name(self, dbt_resource_props: Mapping[str, Any]) -> Optional[str]:
-        return self.group_name
+    def get_group_name(
+            self, dbt_resource_props: Mapping[str, Any]
+        ) -> Optional[str]:
+            return "dbt_alpaca"
 
 @dbt_assets(
     manifest=Path("dbt/target/", "manifest.json"),
-    dagster_dbt_translator=CustomDagsterDbtTranslator("dbt_alpaca"),
-    select="tag:alpaca"
+    dagster_dbt_translator=CustomDagsterDbtTranslator()
 )
 def alpaca_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
