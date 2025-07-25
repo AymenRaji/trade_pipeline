13d23dbca4a198cd5c9d7419eef42ecfef0b2317
rajiaymen <rajiaymen2@gmail.com>
2025-07-25 17:03:31 -0400

    defined new sensor

--- dagster/alpaca_pipeline/__init__.py
+++ dagster/alpaca_pipeline/__init__.py
@@ -7,6 +7,7 @@ from dagster_dbt import DbtCliResource
 from .assets import alpaca_dbt_assets, alpaca_dlt_asset
 from .jobs import Alpaca_ELT_job
 from .schedules import Alpaca_ELT_schedule
+from .sensors import email_on_filure_sensor
 
 alpaca_dlt_asset = load_assets_from_modules([alpaca_dlt_asset])
 alpaca_dbt_asset = load_assets_from_modules([alpaca_dbt_assets])
@@ -29,5 +30,6 @@ defs = Definitions(
         "dbt":dbt_resource
     },
     jobs=[Alpaca_ELT_job],
-    schedules=[Alpaca_ELT_schedule]
+    schedules=[Alpaca_ELT_schedule],
+    sensors=[email_on_filure_sensor]
 )
